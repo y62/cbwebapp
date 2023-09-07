8eb18d9a159145a6331184d221a07f6df4f07cc7
y62 <y62@outlook.dk>
2023-09-07 23:36:56 +0200

    Initial commit

---
+++ cbwebapp/Program.cs
@@ -0,0 +1,7 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:3000");
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
